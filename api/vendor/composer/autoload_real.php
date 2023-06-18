98d49c5e89fae9854f53a19f1066faa5cff93613
Chives <reactchives@gmail.com>
2023-06-17 19:59:22 -0700

    composer require phpoffice/phpspreadsheet

--- api/vendor/composer/autoload_real.php
+++ api/vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInit4703474fbb2f7c93cd3c47536bc5c3e4
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit4703474fbb2f7c93cd3c47536bc5c3e4::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
